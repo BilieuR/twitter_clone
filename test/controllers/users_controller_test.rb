be35823244409b7fba92760f75d71da81182125f
BilieuR <bilieureath1@gmail>
2016-12-27 21:37:04 +1000

    Mock signup forms

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  # test "should get show" do
+  #   get users_show_path
+  #   assert_response :success
+  # end
 end
